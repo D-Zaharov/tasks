28d016fb4a7f438a79344f3e35861e678b6c302a
D-Zaharov <Zdaniil1512@gmail.com>
2019-08-16 14:26:10 +0300

    Скрипт, сортирующий список стран со столицами по алфавиту

---
+++ sorting_list_of_countries/script.py
@@ -0,0 +1,18 @@
+﻿handle = open("countries.txt", "r", encoding="utf-8")
+dictionary_of_countries = {}
+for line in handle:
+    element = line.split(':')
+    dictionary_of_countries[element[0]] = element[1]
+
+handle.close()
+
+list_of_countries = list(dictionary_of_countries.keys())
+list_of_countries.sort()
+
+handle = open("result.txt", "w")
+for country in list_of_countries:
+    capital = dictionary_of_countries.get(country)
+    handle.write(country + ' - ' + capital)
+handle.close
+
+print("список успешно отсортирован и  записан в файл")
